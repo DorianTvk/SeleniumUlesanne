1f26b81049e54209cbdb6ec26fa82a941edd7bfb
Opilane TTHK <opilane@tthk.ee>
2024-10-14 10:50:25 +0300

    Selenium värk esitatud

---
+++ FirstTestCase.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+
+namespace Seleniumülesanne
+{
+    public class FirstTestCase
+    {
+        static void Main(string[] args) 
+        {
+            string binaryLocation = "C:\\Users\\opilane\\source\\repos\\FFP\\FirefoxPortable\\App\\Firefox64\\firefox.exe";
+
+            FirefoxOptions options = new FirefoxOptions();
+            options.BrowserExecutableLocation = binaryLocation;
+
+            IWebDriver driver = new FirefoxDriver("C:\\Users\\opilane\\source\\repos\\Selenium\\Selenium\\Drivers", options);
+            driver.Url = "file:///C:/Users/opilane/source/repos/onnelikud-autod-main/index.html/";
+        }
+
+    }
+}
